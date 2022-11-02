e55380dc9468fe8f67beaab42f07b35d463d9606
LB3368 <latisha2801@gmail.com>
2022-11-02 18:51:48 -0400

    first commit

---
+++ pumpkinMania/js/app.js
@@ -0,0 +1,74 @@
+// Selection of all the CSS selectors
+const score = document.querySelector('.score span')
+const holes = document.querySelectorAll('.hole')
+const start_btn = document.querySelector('.buttons .start')
+console.log(start_btn)
+const stop_btn = document.querySelector('.buttons .stop')
+console.log(stop_btn)
+const cursor = document.querySelector('.hammer img')
+
+// Here we changing our default cursor to hammer
+// (e) refers to event handler
+window.addEventListener('mousemove', (e) => {
+	cursor.style.top = e.pageY + "px"
+	cursor.style.left = e.pageX + "px"
+})
+
+// It is used to give the animation to
+// our hammer every time we click it
+// on our screen
+window.addEventListener('click', () => {
+	cursor.style.animation = 'none'
+	setTimeout(() => {
+		cursor.style.animation = ''
+	}, 101)
+})
+
+// From this part our game starts when
+// we click on the start button
+start_btn.addEventListener('click', () => {
+	start_btn.style.display = 'none'
+	stop_btn.style.display = 'inline-block'
+
+	let holee = [false, false, false, false, false]
+	let points = 0
+
+	const game = setInterval(() => {
+		console.log("game is running")
+		// Here we are taking a random hole
+		// from where pumpkin comes out
+		let ran = Math.floor(Math.random() * 5)
+		holee[ran] = true
+
+		// This part is used for taking the
+		// pumpkin out of the desired hole
+		let set_img = document.createElement('img')
+		set_img.setAttribute('src',
+'../img/halloween-1298060_960_720.png')
+		set_img.setAttribute('class', 'pumpkin')
+		holee.appendChild(set_img)
+
+		// This part is used for putting
+		// the pumpkin back in the hole
+		setTimeout(() => {
+			holee.removeChild(set_img)
+		}, 700);
+	}, 800)
+
+	// It is used for adding our points
+	// to 0 when we hit to the mouse
+	window.addEventListener('click', (e) => {
+		if (e.target === holee)
+			score.innerText = ++points;
+	})
+
+	// This is coded for changing the score
+	// to 0 and change the stop button
+	// again to the start button!
+	stop_btn.addEventListener('click', () => {
+		clearInterval(game)
+		stop_btn.style.display = 'none'
+		start_btn.style.display = 'inline-block'
+		score.innerHTML = '0'
+	})
+})
